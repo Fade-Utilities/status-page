000c8ad2c721b53be98785d65a1e561efdbb652b
Will Clarke <clarke19w@gmail.com>
2022-02-21 22:26:40 +0000

    get browser data and limit interval to firefox while bugs are fixed

--- index.php
+++ index.php
@@ -83,20 +83,49 @@
         <main class="bg-deep-space mt-5">
             <h1 class="text-4xl mb-16 text-center mx-auto text-el-blue font-bold">Will's Uptime</h1>
             <section class="container mx-auto text-el-blue" id="personal-sites-sevices">
-                <div class="container flex flex-row mb-5">
-                    <h1 class="text-2xl mb-5 place-self-start font-bold mx-auto my-auto">Personal Sites & Services</h1>
-                    <div class="object-right place-self-end justify-center mx-auto">
-                            <span class="text-el-blue font-semibold">Change refresh interval: </span><br>
-                            <select id="interval" class="mt-2 hover:cursor-pointer text-el-blue rounded bg-transparent focus:text-black focus:bg-el-blue font-semibold border-el-blue border px-2 py-2 hover:text-black hover:bg-el-blue">
-                                <option class="text-el-blue bg-space" value="10000">10 Seconds</option>
-                                <option class="text-el-blue bg-space" value="20000">20 Seconds</option>
-                                <option class="text-el-blue bg-space" selected value="30000">30 Seconds</option>
-                                <option class="text-el-blue bg-space" value="45000">45 Seconds</option>
-                                <option class="text-el-blue bg-space" value="60000">1 Minute</option>
-                            </select>
+                <?php
+                function browserdata(){
+                    if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false)
+                        return 'Internet explorer';
+                    elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== false)
+                            return 'Internet explorer';
+                    elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== false)
+                        return 'Mozilla Firefox';
+                    elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== false)
+                        return 'Google Chrome';
+                    elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== false)
+                        return "Opera Mini";
+                    elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== false)
+                        return "Opera";
+                    elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Safari') !== false)
+                        return "Safari";
+                    else
+                        return 'Other';
+                }
+                $browser = browserdata();
+                $true = true;
+                if ($browser == 'Mozilla Firefox' /*$true*/){
+                    ?>
+                    <div class="flex flex-row mb-5 justify-between w-full">
+                        <h1 class="text-2xl mb-5 place-self-start font-bold mx-auto my-auto">Personal Sites & Services</h1>
+                        <div class="object-right place-self-end justify-center mx-auto">
+                                <span class="text-el-blue font-semibold">Change refresh interval: </span><br>
+                                <select id="interval" class="mt-2 hover:cursor-pointer text-el-blue rounded bg-transparent focus:text-black focus:bg-el-blue font-semibold border-el-blue border px-2 py-2 hover:text-black hover:bg-el-blue">
+                                    <option class="text-el-blue bg-space" value="10000">10 Seconds</option>
+                                    <option class="text-el-blue bg-space" value="20000">20 Seconds</option>
+                                    <option class="text-el-blue bg-space"  value="30000">30 Seconds</option>
+                                    <option class="text-el-blue bg-space" value="45000">45 Seconds</option>
+                                    <option class="text-el-blue bg-space" value="60000">1 Minute</option>
+                                </select>
+                        </div>
                     </div>
-                </div>
-                <?php while ($i < $sitearr_len){
+                    <?php
+                } else {
+                    ?>
+                    <h1 class="text-2xl mb-5 font-bold">Personal Sites & Services</h1>
+                    <?php
+                }
+                while ($i < $sitearr_len){
                     ?><div class="bg-space rounded flex flex-row" id="<?php echo $i."-Sec";?>"><?php
                         $url = $arr_sites[$i][0];
                         $status = $arr_sites[$i][1];
